96b758e8f1b013eea95317f8151c4f1c0d8e282b
23102040237 <23102040237@github.com>
2024-12-10 22:41:30 +0800

    te update

--- src/main/java/org/example/NumberFeature.java
+++ src/main/java/org/example/NumberFeature.java
@@ -5,7 +5,7 @@ import java.util.Scanner;
 public class NumberFeature {
     public static void main(String[] args) {
         int inputNumber; // 输入的数字
-        int result = 0; // 输出结果
+        int result; // 输出结果
         Scanner in = new Scanner(System.in);
         inputNumber = in.nextInt();
         // 你的代码逻辑
